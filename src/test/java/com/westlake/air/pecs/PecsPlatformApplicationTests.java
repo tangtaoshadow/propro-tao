ad7ab24a40ff3e5dcb61f11a0b6cfb4f274aa020
James <lms19880914@qq.com>
2018-07-20 13:30:11 +0800

    平台由SwathPlatform正式更名为PECS

---
+++ src/test/java/com/westlake/air/pecs/PecsPlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.westlake.air.pecs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PecsPlatformApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
