3cdc1f5e243546d829f92ef4b570c399126f7ae3
anshaowei <794990124@qq.com>
2018-08-15 09:44:37 +0800

    添加了小部分测试用例壳子

--- src/test/java/com/westlake/air/pecs/test/algorithm/AirusTest.java
+++ src/test/java/com/westlake/air/pecs/test/algorithm/AirusTest.java
@@ -33,4 +33,5 @@ public class AirusTest extends BaseTest {
             assert true;
         }
     }
+
 }
--- src/test/java/com/westlake/air/pecs/test/dao/TransitionDAOTest.java
+++ src/test/java/com/westlake/air/pecs/test/dao/TransitionDAOTest.java
@@ -2,6 +2,7 @@ package com.westlake.air.pecs.test.dao;
 
 import com.westlake.air.pecs.dao.TransitionDAO;
 import com.westlake.air.pecs.domain.db.TransitionDO;
+import com.westlake.air.pecs.domain.query.TransitionQuery;
 import com.westlake.air.pecs.test.BaseTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +19,80 @@ public class TransitionDAOTest extends BaseTest {
     TransitionDAO transitionDAO;
 
     @Test
-    public void 测试根据LibraryId获取所有Transition列表() {
+    public void getAllByLibraryIdTest() {
         List<TransitionDO> list = transitionDAO.getAllByLibraryId("5b6712012ada5f2dc8de57d7");
-        assert(list.size()>0);
+        assert true;
     }
+
+    @Test
+    public void getTransitionGroupIdsTest() {
+        List<String> result = transitionDAO.getTransitionGroupIds("5b67136d2ada5f15749a0140", "AGVETTTPSK_2");
+        List<TransitionDO> list = transitionDAO.getAllByLibraryId("5b6712012ada5f2dc8de57d7");
+        assert true;
+    }
+
+    @Test
+    public void getAllByLibraryIdAndIsDecoy() {
+        List<TransitionDO> result = transitionDAO.getAllByLibraryIdAndIsDecoy("5b67136d2ada5f15749a0140", false);
+        assert true;
+    }
+
+    @Test
+    public void getListTest() {
+        TransitionDO transitionDO = getJsonFromFileTest();
+        TransitionQuery transitionQuery = new TransitionQuery();
+        List<TransitionDO> result = transitionDAO.getList(transitionQuery);
+        assert true;
+    }
+
+    @Test
+    public void getTTAllTest() {
+        assert true;
+    }
+
+    @Test
+    public void countTest() {
+        assert true;
+    }
+
+    @Test
+    public void getByIdTest() {
+        assert true;
+    }
+
+    @Test
+    public void insertTest_TransitionDO() {
+        assert true;
+    }
+
+    @Test
+    public void insertTest_ListTransitionDO() {
+        assert true;
+    }
+
+    @Test
+    public void updateTest() {
+        assert true;
+    }
+
+    @Test
+    public void deleteTest() {
+        assert true;
+    }
+
+    @Test
+    public void deleteAllByLibraryIdTest(){
+        assert true;
+    }
+
+    @Test
+    public void getProteinListTest(){
+        assert true;
+    }
+
+    @Test
+    public void getPeptideListTest(){
+        assert true;
+    }
+
 }
