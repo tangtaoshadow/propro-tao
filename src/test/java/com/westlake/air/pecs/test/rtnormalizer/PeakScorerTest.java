9b505a5708081696d36ec33a1cea63ed60080d9b
anshaowei <794990124@qq.com>
2018-08-15 16:59:16 +0800

    修改了之前expect==result的情况,有些时候可能会造成错误

---
+++ src/test/java/com/westlake/air/pecs/test/rtnormalizer/PeakScorerTest.java
@@ -0,0 +1,45 @@
+package com.westlake.air.pecs.test.rtnormalizer;
+
+import com.westlake.air.pecs.domain.bean.score.ScoreRtPair;
+import com.westlake.air.pecs.rtnormalizer.PeakScorer;
+import com.westlake.air.pecs.test.BaseTest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @Author: An Shaowei
+ * @Time: 2018/8/15 9:50
+ */
+public class PeakScorerTest extends BaseTest {
+    @Autowired
+    PeakScorer peakScorer;
+
+    @Test
+    public void scoreTest(){
+        List<ScoreRtPair> result;
+        assert true;
+    }
+
+    @Test
+    public void calculateChromatographicScoresTest(){
+        assert true;
+    }
+
+    @Test
+    public void calculateLibraryScoresTest(){
+        assert true;
+    }
+
+    @Test
+    public void calculateIntensityScoreTest(){
+        assert true;
+    }
+
+    @Test
+    public void calculateDiaMassDiffScoreTest(){
+        assert true;
+    }
+
+}
