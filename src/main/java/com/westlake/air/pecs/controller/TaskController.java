378a5c7383bcaacc9f7e17c5b1ec1990b25aba89
James <lms19880914@qq.com>
2018-08-15 20:54:06 +0800

    更新首页

--- src/main/java/com/westlake/air/pecs/controller/TaskController.java
+++ src/main/java/com/westlake/air/pecs/controller/TaskController.java
@@ -1,8 +1,13 @@
 package com.westlake.air.pecs.controller;
 
+import com.westlake.air.pecs.constants.TaskTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 /**
  * Created by James Lu MiaoShan
@@ -13,8 +18,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TaskController extends BaseController{
 
     @RequestMapping(value = "/create")
-    String create(Model model) {
+    String create(Model model,
+                  @RequestParam(value = "templateName", required = true) String templateName) {
+        TaskTemplate template = TaskTemplate.getByName(templateName);
+        if(template != null){
+            return "redirect:"+template.getPagePath();
+        }else{
+            return "/home";
+        }
+    }
 
-        return "task/create";
+    @RequestMapping(value = "/detail/{id}")
+    String detail(Model model) {
+        return "task/detail";
+    }
+
+    @RequestMapping(value = "/update/{id}")
+    String update(Model model) {
+        return "task/detail";
+    }
+
+    @RequestMapping(value = "/delete/{id}")
+    String delete(Model model) {
+        return "task/detail";
     }
 }
