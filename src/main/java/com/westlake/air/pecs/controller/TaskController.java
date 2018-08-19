a5e8b630533c4c56b15b53513ac06a3973db3a30
James <lms19880914@qq.com>
2018-08-19 19:26:49 +0800

    任务列表页面开发完毕

--- src/main/java/com/westlake/air/pecs/controller/TaskController.java
+++ src/main/java/com/westlake/air/pecs/controller/TaskController.java
@@ -4,7 +4,10 @@ import com.alibaba.fastjson.JSON;
 import com.westlake.air.pecs.constants.ResultCode;
 import com.westlake.air.pecs.constants.TaskTemplate;
 import com.westlake.air.pecs.domain.ResultDO;
+import com.westlake.air.pecs.domain.db.ExperimentDO;
 import com.westlake.air.pecs.domain.db.TaskDO;
+import com.westlake.air.pecs.domain.query.ExperimentQuery;
+import com.westlake.air.pecs.domain.query.TaskQuery;
 import com.westlake.air.pecs.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -27,9 +31,20 @@ public class TaskController extends BaseController {
     TaskService taskService;
 
     @RequestMapping(value = "/list")
-    String list(Model model) {
-//        taskService.getList()
-        return null;
+    String list(Model model,
+                @RequestParam(value = "currentPage", required = false, defaultValue = "1") Integer currentPage,
+                @RequestParam(value = "pageSize", required = false, defaultValue = "20") Integer pageSize) {
+
+        model.addAttribute("pageSize", pageSize);
+        TaskQuery query = new TaskQuery();
+        query.setPageSize(pageSize);
+        query.setPageNo(currentPage);
+        ResultDO<List<TaskDO>> resultDO = taskService.getList(query);
+
+        model.addAttribute("tasks", resultDO.getModel());
+        model.addAttribute("totalPage", resultDO.getTotalPage());
+        model.addAttribute("currentPage", currentPage);
+        return "task/list";
     }
 
     @RequestMapping(value = "/create")
@@ -54,7 +69,7 @@ public class TaskController extends BaseController {
         model.addAttribute("task", taskDO);
         model.addAttribute("taskId", taskDO.getId());
         TaskTemplate taskTemplate = TaskTemplate.getByName(taskDO.getTaskTemplate());
-        if(taskTemplate == null){
+        if (taskTemplate == null) {
             model.addAttribute(ERROR_MSG, ResultCode.OBJECT_NOT_EXISTED.getMessage());
             return "task/detail";
         }
@@ -65,9 +80,9 @@ public class TaskController extends BaseController {
     @ResponseBody
     String getTaskInfo(Model model, @PathVariable("id") String id) {
         ResultDO<TaskDO> resultDO = taskService.getById(id);
-        if(resultDO.isSuccess() && resultDO.getModel() != null){
+        if (resultDO.isSuccess() && resultDO.getModel() != null) {
             return JSON.toJSONString(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(resultDO.getModel().getLastModifiedDate()));
-        }else{
+        } else {
             return null;
         }
     }
