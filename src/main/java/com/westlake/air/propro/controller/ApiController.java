8e5646c410e3e1631e1e07d495578b7b25f1f986
lms <lms19880914@qq.com>
2019-01-24 15:18:15 +0800

    更新一段CSS脚本用于优化界面展示

--- src/main/java/com/westlake/air/propro/controller/ApiController.java
+++ src/main/java/com/westlake/air/propro/controller/ApiController.java
@@ -2,8 +2,11 @@ package com.westlake.air.propro.controller;
 
 import com.westlake.air.propro.domain.ResultDO;
 import com.westlake.air.propro.domain.db.ExperimentDO;
+import com.westlake.air.propro.domain.db.LibraryDO;
 import com.westlake.air.propro.domain.query.ExperimentQuery;
+import com.westlake.air.propro.domain.query.LibraryQuery;
 import com.westlake.air.propro.service.ExperimentService;
+import com.westlake.air.propro.service.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,17 +22,35 @@ public class ApiController {
 
     @Autowired
     ExperimentService experimentService;
+    @Autowired
+    LibraryService libraryService;
 
+    /**
+     * 根据ID获取实验对象
+     * @param model
+     * @param id
+     * @return
+     */
     @ResponseBody
-    @RequestMapping(value = "getExperimentById")
+    @RequestMapping(value = "experiment/getById")
     public ResultDO<ExperimentDO> getExperimentById(Model model,
                                          @RequestParam(value = "id", required = true) String id) {
         ResultDO<ExperimentDO> resultDO = experimentService.getById(id);
         return resultDO;
     }
 
+    /**
+     * 根据条件批量分页获取实验列表
+     * @param model
+     * @param currentPage
+     * @param pageSize
+     * @param projectName
+     * @param batchName
+     * @param expName
+     * @return
+     */
     @ResponseBody
-    @RequestMapping(value = "getExperimentList")
+    @RequestMapping(value = "experiment/getList")
     public ResultDO<List<ExperimentDO>> getExperimentList(Model model,
                     @RequestParam(value = "currentPage", required = false, defaultValue = "1") Integer currentPage,
                     @RequestParam(value = "pageSize", required = false, defaultValue = "50") Integer pageSize,
@@ -50,6 +71,37 @@ public class ApiController {
         query.setPageNo(currentPage);
         ResultDO<List<ExperimentDO>> resultDO = experimentService.getList(query);
 
+        return resultDO;
+    }
+
+    /**
+     * 根据ID获取标准库对象(包含iRT库)
+     * @param model
+     * @param id
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "library/getById")
+    public ResultDO<LibraryDO> getLibraryById(Model model,
+                                              @RequestParam(value = "id", required = true) String id) {
+        ResultDO<LibraryDO> resultDO = libraryService.getById(id);
+        return resultDO;
+    }
+
+    /**
+     * 根据ID获取标准库对象(包含iRT库)
+     * @param model
+     * @param currentPage
+     * @param pageSize
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "library/getList")
+    public ResultDO<List<LibraryDO>> getLibraryById(Model model,
+                                                    @RequestParam(value = "currentPage", required = false, defaultValue = "1") Integer currentPage,
+                                                    @RequestParam(value = "pageSize", required = false, defaultValue = "50") Integer pageSize) {
+        LibraryQuery libraryQuery = new LibraryQuery();
+        ResultDO<List<LibraryDO>> resultDO = libraryService.getList(libraryQuery);
         return resultDO;
     }
 }
