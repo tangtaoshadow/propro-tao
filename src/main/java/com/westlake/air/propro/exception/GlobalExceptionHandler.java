aa516d5eafa5f97d73fc468c6e55498be640b676
lms <lms19880914@qq.com>
2019-05-22 18:37:27 +0800

    增加对于未授权访问的资源进行访问时的报错信息

--- src/main/java/com/westlake/air/propro/exception/GlobalExceptionHandler.java
+++ src/main/java/com/westlake/air/propro/exception/GlobalExceptionHandler.java
@@ -1,20 +1,22 @@
 package com.westlake.air.propro.exception;
 
+import com.westlake.air.propro.constants.ResultCode;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotLoginException.class)
-    public String userNotLoginException(UserNotLoginException e, HttpServletResponse response) {
+    public String userNotLoginException(UserNotLoginException e, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("error_msg", ResultCode.USER_NOT_EXISTED.getMessage());
         return "redirect:/login/login";
     }
 
     @ExceptionHandler(UnauthorizedAccessException.class)
-    public String unauthorizedAccessException(UnauthorizedAccessException e, HttpServletResponse response) {
+    public String unauthorizedAccessException(UnauthorizedAccessException e, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("error_msg", ResultCode.UNAUTHORIZED_ACCESS.getMessage());
         return e.getRedirectPage();
     }
 }
