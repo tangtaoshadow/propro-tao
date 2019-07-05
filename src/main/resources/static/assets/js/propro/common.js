e1137e0eb7ad519f54a9bdf4e588c0f27917a0a7
a15058713697 <2393264074@qq.com>
2019-07-05 10:44:06 +0800

    高级选项和语言切换按钮更新

---
+++ src/main/resources/static/assets/js/propro/common.js
@@ -0,0 +1,47 @@
+
+var changeBtn=$(".change").eq(0);
+//change language begin
+var change=function () {
+    var langCh=$(".Chinese").eq(0);
+    var langEn=$(".English").eq(0);
+    var url=window.location.href.split("?")[0];
+    console.log("已经执行change");
+    langCh.attr("href",url+"?"+"lang=ch");
+    langEn.attr("href",url+"?"+"lang=en");
+    // var changeB=function () {
+    //     var text=$(this).text();
+    //     $(this).text(text);
+    //     return false
+    // };
+    // langCh.on("click",changeB);
+    // langEn.on("click", changeB);
+};
+changeBtn.hover(change);
+
+//change language end
+
+
+//high level choose begin
+var highPower=$("#m_accordion_1_item_1_head");
+var icon=$("#icon");
+var newAdd=function () {
+    var changeIcon=function () {
+        if(icon.attr("class").indexOf("fa-chevron-down")<0){
+            icon.removeClass("fa-chevron-up");
+            icon.addClass("fa-chevron-down");
+        }else{
+            icon.removeClass("fa-chevron-down");
+            icon.addClass("fa-chevron-up");
+        }
+    };
+    //high level choose end
+    return {
+        init:function () {
+            changeIcon();
+
+        }
+    }
+}();
+highPower.click(function() {
+    newAdd.init();
+});
